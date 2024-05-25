855f4027b399a2a5b3d011c962e7751c12fe7192
GayathriH90 <rajiishri@gmail.com>
2024-05-26 02:24:08 +0530

    corrected Mock_server.py

--- DataPusher/DataPusher/mock_server.py
+++ DataPusher/DataPusher/mock_server.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 def api():
     headers = dict(request.headers)
     data = request.get_json()
-    #print(data)
+    print(data)
     return jsonify({"headers": headers, "data": data})
 
-if __name__ == '__main__'
+if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0',port=8081)
